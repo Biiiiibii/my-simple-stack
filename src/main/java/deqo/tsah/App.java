ae678a416f02127b94de229ba7b64565502d3d50
Biiiiibii <tugay.sahin@univ-tlse3.fr>
2017-10-19 11:41:23 +0200

    Premier commit

---
+++ src/main/java/deqo/tsah/App.java
@@ -0,0 +1,13 @@
+package deqo.tsah;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
